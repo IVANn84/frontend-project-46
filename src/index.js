b25067323ee9fe970116b6d007e71fa6c7da2b09
Ivan Khristenko <ivan.tihii1984@gmail.com>
2022-09-13 10:59:01 +0300

    3 step_gendiff

--- src/index.js
+++ src/index.js
@@ -5,10 +5,52 @@ import _ from 'lodash';
 
 const getfilePath = (filePath) => resolve(cwd(), filePath);
 
-const readFile = (path) => readFileSync(path);
+const readFile = (path) => readFileSync(path, 'utf-8');
 
 const parsesFile = (file) => JSON.parse(file);
 
+const getDiffInformation = (data1, data2) => {
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+
+  const keys = _.sortBy(_.union(keys1, keys2));
+
+  const result = keys.map((key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
+
+    if (_.isEqual(value1, value2)) {
+      return {
+        type: 'unchanges',
+        key,
+        value: value1,
+      };
+    }
+    if (value1 && value2 && value1 !== value2) {
+      return {
+        type: 'changet',
+        key,
+        value1,
+        value2,
+      };
+    }
+    if (!Object.hasOwn(data2, key)) {
+      return {
+        type: 'delited',
+        key,
+        value: value1,
+      };
+    }
+    if (!Object.hasOwn(data1, key)) {
+      return {
+        type: 'added',
+        key,
+        value: value2,
+      };
+    }
+  });
+  return result;
+};
 const gendiff = (filePath1, filePath2) => {
   const path1 = getfilePath(filePath1);
   const path2 = getfilePath(filePath2);
@@ -18,32 +60,26 @@ const gendiff = (filePath1, filePath2) => {
 
   const data1 = parsesFile(file1);
   const data2 = parsesFile(file2);
-  console.log(data1);
-  console.log(data2);
 
-  const keys1 = Object.keys(data1);
-  const keys2 = Object.keys(data2);
-
-  const keys = _.sortBy(_.union(keys1, keys2));
-  console.log(keys);
-  const result = {};
-
-  for (const key of keys) {
-    if (_.has(data1, [key])) {
-      result[`- ${key}`] = data1[key];
-    } else if (_.has(data2, [key])) {
-      result[`+ ${key}`] = data2[key];
-    } else if (
-      _.has(data1, [key]) &&
-      _.has(data2, [key] && data1[key] === data2[key])
-    ) {
-      result[key] = data1[key];
+  const informationDiff = getDiffInformation(data1, data2);
+  console.log(informationDiff);
+  const result = informationDiff.map((diff) => {
+    const typeDiff = diff.type;
+    switch (typeDiff) {
+      case 'delited':
+        return `- ${diff.key}: ${diff.value}`;
+      case 'unchanges':
+        return `  ${diff.key}: ${diff.value}`;
+      case 'changet':
+        return `- ${diff.key}: ${diff.value1} \n +${diff.key}: ${diff.value2}`;
+      case 'added':
+        return `+ ${diff.key}: ${diff.value1}`;
     }
-  }
+  });
   console.log(result);
-
-  return result;
+  return `{\n${result.join('\n')}\n}`;
 };
 
 gendiff('../__fixtures__/file1.json', '../__fixtures__/file2.json');
+
 export default gendiff;
