f3b20f6ec5f51ec7d6bbacdd3107b3d0674ec78a
Ivan Khristenko <ivan.tihii1984@gmail.com>
2023-01-25 16:50:35 +0300

    4 step test coverage adding

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,10 +6,8 @@ import gendiff from '../src/index.js';
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) =>
-  path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) =>
-  fs.readFileSync(getFixturePath(filename), 'utf-8');
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const fileOutput = readFile('fileOutput.txt');
 const file1 = './__fixtures__/file1.json';
